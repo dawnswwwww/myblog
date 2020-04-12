635c39f8363333e3ab43ceb3007ca44f825064b6
王晓楠 <wxn_em@163.com>
2020-04-12 12:18:08 +0800

    init

---
+++ myblog/precache-manifest.8554929ff170d097ce929d73520aaf7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a86fbe90b96645049580",
+    "url": "/css/app.904f8d21.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "c6401fd705ee50905944019ee729b81a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0b8f2988c85113f9a38",
+    "url": "/js/about.de781b12.js"
+  },
+  {
+    "revision": "a86fbe90b96645049580",
+    "url": "/js/app.85b26d0d.js"
+  },
+  {
+    "revision": "491201c027b03ea99a2a",
+    "url": "/js/chunk-vendors.d296baf7.js"
+  },
+  {
+    "revision": "630418935502ad2382b3f569bd9d27dd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
